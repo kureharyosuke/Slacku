79ac424d82c55fe65ec3c4984c8f1c7b39e6d86a
kureharyosuke <ohhayo.com@gmail.com>
2021-06-26 14:26:34 +0900

    Install: react-refresh fork-ts-checker-webpack-plugin

--- react-frontend/webpack.config.ts
+++ react-frontend/webpack.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
-// import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import webpack, { Configuration as WebpackConfiguration } from 'webpack';
-// import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 // import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 // import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 
@@ -63,42 +63,42 @@ const config: Configuration = {
     ],
   },
   plugins: [
-    // new ForkTsCheckerWebpackPlugin({
-    //   async: false,
-    //   // eslint: {
-    //   //   files: "./src/**/*",
-    //   // },
-    // }),
-    // new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      //   // eslint: {
+      //   //   files: "./src/**/*",
+      // }
+    }),
+    new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
     publicPath: '/dist/',
   },
-  // devServer: {
-  //   historyApiFallback: true,
-  //   port: 3090,
-  //   publicPath: '/dist/',
-  //   proxy: {
-  //     '/api/': {
-  //       target: 'http://localhost:3095',
-  //       changeOrigin: true,
-  //       ws: true,
-  //     },
-  //   },
-  // },
+  devServer: {
+    historyApiFallback: true, // react router 설정할때 필요하다.
+    port: 3090,
+    publicPath: '/dist/',
+    //   proxy: {
+    //     '/api/': {
+    //       target: 'http://localhost:3095',
+    //       changeOrigin: true,
+    //       ws: true,
+    //     },
+    //   },
+  },
 };
 
 if (isDevelopment && config.plugins) {
-  // config.plugins.push(new webpack.HotModuleReplacementPlugin());
-  // config.plugins.push(
-  //   new ReactRefreshWebpackPlugin({
-  //     overlay: {
-  //       useURLPolyfill: true,
-  //     },
-  //   }),
-  // );
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  config.plugins.push(
+    new ReactRefreshWebpackPlugin({
+      overlay: {
+        useURLPolyfill: true,
+      },
+    }),
+  );
   // config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: false }));
 }
 if (!isDevelopment && config.plugins) {
